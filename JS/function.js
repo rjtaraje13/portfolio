36547d75085ba7a3b9ebba1b93a20ad8636b8fe7
rjtaraje13 <relixer.zr@gmail.com>
2024-04-10 19:16:07 +0800

    updaated

---
+++ JS/function.js
@@ -0,0 +1,17 @@
+document.getElementById('proj').addEventListener('click', function() {
+  // Redirect to the next page
+  window.location.href = '../HTML/projects.html';
+  console.log("yey")
+}); 
+
+document.getElementById('home').addEventListener('click', function() {
+  // Redirect to the next page
+  window.location.href = '../index.html';
+}); 
+
+document.getElementById('abt').addEventListener('click', function() {
+  // Redirect to the next page
+  console.log("yey")
+  window.location.href = '../HTML/about.html';
+}); 
+
