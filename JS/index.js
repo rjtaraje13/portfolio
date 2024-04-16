4271ab1738022a0a5ee226a532d5c6ef8c286a4a
rjtaraje13 <relixer.zr@gmail.com>
2024-04-16 20:10:51 +0800

    add js

--- JS/index.js
+++ JS/index.js
@@ -1,16 +1,16 @@
 document.getElementById('proj').addEventListener('click', function() {
   // Redirect to the next page
-  window.location.href = '../HTML/projects.html';
+  window.location.href = './HTML/projects.html';
   
 }); 
 
 document.getElementById('home').addEventListener('click', function() {
   // Redirect to the next page
-  window.location.href = '../index.html';
+  window.location.href = './index.html';
 }); 
 
 document.getElementById('abt').addEventListener('click', function() {
   // Redirect to the next page
-  window.location.href = '../HTML/about.html';
+  window.location.href = './HTML/about.html';
 }); 
 
